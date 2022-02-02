cb3137893de186e80671866ef3625e2e8b1b394b
RTX_LW <59782142+LeeWen0802@users.noreply.github.com>
2022-02-02 21:10:55 +0800

    New

---
+++ docusaurus.config.js
@@ -0,0 +1,74 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+    title: 'Mino Host Docs',
+    tagline: 'Documentation for Mino Host',
+    url: 'https://docs.mino.host',
+    baseUrl: '/',
+    onBrokenLinks: 'throw',
+    onBrokenMarkdownLinks: 'warn',
+    favicon: 'img/favicon.ico',
+    organizationName: 'Mino-Host', // Usually your GitHub org/user name.
+    projectName: 'Docs', // Usually your repo name.
+
+    presets: [
+        [
+            'classic',
+            /** @type {import('@docusaurus/preset-classic').Options} */
+            ({
+                docs: {
+                    routeBasePath: '/',
+                    sidebarPath: require.resolve('./sidebars.js'),
+                    // Please change this to your repo.
+                    editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+                },
+                blog: false,
+                theme: {
+                    customCss: require.resolve('./src/css/custom.css'),
+                },
+            }),
+        ],
+    ],
+
+    themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+        ({
+        hideableSidebar: true,
+        navbar: {
+            title: 'Mino Host Docs',
+            logo: {
+                alt: 'Mino Host Logo',
+                src: 'img/logo.svg',
+            },
+            items: [{
+                    type: 'localeDropdown',
+                    position: 'left',
+                },
+                {
+                    href: 'https://github.com/Mino-Host/Docs',
+                    label: 'GitHub',
+                    position: 'right',
+                },
+            ],
+        },
+        footer: {
+            style: 'dark',
+            copyright: `Copyright © ${new Date().getFullYear()} Mino Host.`,
+        },
+        prism: {
+            theme: lightCodeTheme,
+            darkTheme: darkCodeTheme,
+        },
+    }),
+    i18n: {
+        defaultLocale: 'en',
+        locales: ['en', 'zh-tw'],
+    },
+};
+
+module.exports = config;
